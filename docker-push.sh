94f4fb99ac3e349af4065900b627158a92773df1
Roberto Focosi <roberto.focosi@ifood.com.br>
2019-11-22 22:02:07 -0300

    Refactor

--- docker-push.sh
+++ docker-push.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 HUB_REPOSITORY=rfocosi/synchronet-bbs
 
-docker build --build-arg BUILD_TYPE=minimal -t $HUB_REPOSITORY:minimal .
+docker build -f Dockerfile-base -t $HUB_REPOSITORY:base .
+[ "$?" != "0" ] && exit 1
+docker push $HUB_REPOSITORY:base
+
+docker build -f Dockerfile-minimal -t $HUB_REPOSITORY:minimal .
 [ "$?" != "0" ] && exit 1
 docker push $HUB_REPOSITORY:minimal
 
-docker build --build-arg BUILD_TYPE=all -t $HUB_REPOSITORY:all .
+docker build -f Dockerfile-all -t $HUB_REPOSITORY:all .
 [ "$?" != "0" ] && exit 1
 docker push $HUB_REPOSITORY:all
