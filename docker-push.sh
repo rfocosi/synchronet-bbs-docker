e8cb15b0443736ea9d86d8a786b9b143d5618aa6
Roberto Focosi <roberto.focosi@ifood.com.br>
2019-11-22 22:26:52 -0300

    Update docker-push

--- docker-push.sh
+++ docker-push.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 HUB_REPOSITORY=rfocosi/synchronet-bbs
 
-docker build -f Dockerfile-base -t $HUB_REPOSITORY:base .
-[ "$?" != "0" ] && exit 1
-docker push $HUB_REPOSITORY:base
-
 docker build -f Dockerfile-minimal -t $HUB_REPOSITORY:minimal .
 [ "$?" != "0" ] && exit 1
 docker push $HUB_REPOSITORY:minimal
 
 docker build -f Dockerfile-all -t $HUB_REPOSITORY:all .
 [ "$?" != "0" ] && exit 1
+docker tag $HUB_REPOSITORY:all $HUB_REPOSITORY:latest
 docker push $HUB_REPOSITORY:all
+docker push $HUB_REPOSITORY:latest
